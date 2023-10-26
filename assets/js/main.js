dae7d96aaa1deea541848575a6f2e6bbbb4e1c0d
yuri.jpereira <yuri.j.pereira2001@gmail.com>
2023-10-26 15:33:26 -0300

    atualizando

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,24 @@ function reinicia() {
     display.value = ""
 }
 
-function recebeValor(valor) {
-    display.value += valor
+function tela(valor) {
+    const sinaisDuplicados = new RegExp(/[\+\-\*\/\.]$/)
+    const ultimoCaractereSinal = sinaisDuplicados.test(display.value)
+    const valorDigitadoSinal = sinaisDuplicados.test(valor)
+
+    // caso o usuário inicie com um ponto, é adicionado zero antes
+    if(display.value === '.'){
+        display.value = '0.'
+    }
+    
+    if (display.value === '/' || display.value === '*' || display.value === '+') {
+        reinicia()
+    }
+
+    // evita sinais duplicados em seguida
+    if (!ultimoCaractereSinal || !valorDigitadoSinal) {
+        display.value += valor
+    }
 }
 
 function deleta() {
@@ -14,5 +30,16 @@ function deleta() {
 
 function calcular() {
     let resultado = eval(display.value)
-    display.value = resultado
+    
+    // evita operações inválidas
+    if(resultado === undefined){
+        display.value = '0'
+    } else if (resultado === Infinity || isNaN(resultado)) {
+        alert("Operação matemática inválida")
+        reinicia()
+    } else {
+        display.value = resultado
+    }
 }
+
+console.log()
