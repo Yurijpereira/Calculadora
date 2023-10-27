bc8440a7b5b7fd358bb6928784127d5a3fb39f57
yuri.jpereira <yuri.j.pereira2001@gmail.com>
2023-10-27 11:09:13 -0300

    atualizando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,30 @@
 const display = document.querySelector("input#display")
+let ponto = true
 
 function reinicia() {
+    ponto = true
     display.value = ""
 }
 
+function verificaPonto(valor) {
+    if(valor === '+' || valor === '-' || valor === '/' || valor === '*') {
+        ponto = true
+    } else if (valor === '.'){
+        ponto = false
+    }
+}
+
+function removerPontoDuplicado() {
+    if(!ponto){
+        deleta()
+    }
+}
+
 function tela(valor) {
-    const sinaisDuplicados = new RegExp(/[\+\-\*\/\.]$/)
+    const sinaisDuplicados = new RegExp(/[\+\-\*\/]$/)
     const ultimoCaractereSinal = sinaisDuplicados.test(display.value)
     const valorDigitadoSinal = sinaisDuplicados.test(valor)
-
+    
     // caso o usuário inicie com um ponto, é adicionado zero antes
     if(display.value === '.'){
         display.value = '0.'
@@ -17,16 +33,22 @@ function tela(valor) {
     if (display.value === '/' || display.value === '*' || display.value === '+') {
         reinicia()
     }
-
+    
     // evita sinais duplicados em seguida
     if (ultimoCaractereSinal && valorDigitadoSinal) {
         return
     }
     
     display.value += valor
+    if(valor === '.'){
+        removerPontoDuplicado()
+    }
+    verificaPonto(valor)
 }
 
+
 function deleta() {
+    ponto = true
     display.value = display.value.slice(0, -1)
 }
 
