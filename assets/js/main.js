8eb917dbb9008bb66dd3d265afa87caf6d32de3c
yuri.jpereira <yuri.j.pereira2001@gmail.com>
2023-10-30 11:36:47 -0300

    atualizando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,82 +1,85 @@
-const display = document.querySelector("input#display")
-let ponto = true
+const display = document.querySelector("input#display");
+let ponto = true;
 
 function reinicia() {
-    ponto = true
-    display.value = ""
+    ponto = true;
+    display.value = "";
 }
 
 function verificaPonto(valor) {
     if(valor === '+' || valor === '-' || valor === '/' || valor === '*') {
-        ponto = true
+        ponto = true;
     } else if (valor === '.'){
-        ponto = false
+        ponto = false;
     }
 }
 
 function removerPontoDuplicado() {
     if(!ponto){
-        deleta()
+        deleta();
     }
 }
 
 function tela(valor) {
-    const sinaisDuplicados = new RegExp(/[\+\-\*\/]$/)
-    const ultimoCaractereSinal = sinaisDuplicados.test(display.value)
-    const valorDigitadoSinal = sinaisDuplicados.test(valor)
+    const sinaisDuplicados = new RegExp(/[\+\-\*\/]$/);
+    const ultimoCaractereSinal = sinaisDuplicados.test(display.value);
+    const valorDigitadoSinal = sinaisDuplicados.test(valor);
     
     // caso o usuário inicie com um ponto, é adicionado zero antes
     if(display.value === '.'){
-        display.value = '0.'
+        display.value = '0.';
     }
     
     // evita iniciar com operadores sem números antes
     if (display.value === '/' || display.value === '*' || display.value === '+') {
-        reinicia()
+        reinicia();
     }
     
     // evita sinais duplicados em seguida
     if (ultimoCaractereSinal && valorDigitadoSinal) {
-        return
+        return;
     }
 
     // limita a quantidade de caracteres no display
     if (display.value.length === 12) {
-        return
+        return;
     }
     
-    display.value += valor
+    display.value += valor;
 
     // evita mais de um ponto no mesmo número
     if (valor === '.'){
-        removerPontoDuplicado()
+        removerPontoDuplicado();
     }
 
-    verificaPonto(valor)
+    verificaPonto(valor);
 }
 
 
 function deleta() {
-    ponto = true
-    display.value = display.value.slice(0, -1)
+    ponto = true;
+    display.value = display.value.slice(0, -1);
 }
 
 function calcular() {
-    let resultado = eval(display.value)
-    
-    if(display.value.includes(".")) {
-        ponto = false
-    } else {
-        ponto = true
-    }
+    let resultado = eval(display.value);
     
     // evita operações inválidas
     if(resultado === undefined){
-        display.value = '0'
+        display.value = '0';
     } else if (resultado === Infinity || isNaN(resultado) || resultado === -Infinity) {
-        alert("Operação matemática inválida")
-        reinicia()
+        alert("Operação matemática inválida");
+        reinicia();
     } else {
-        display.value = resultado
+        display.value = resultado;
     }
+}
+
+function verificaPontoAposCalculo() {
+    if(display.value.includes(".")) {
+        ponto = false;
+    } else {
+        ponto = true;
+    }
+    console.log(display.value)
 }
